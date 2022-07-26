1fa4e14f3e2e0da85a4c7109b33353638c79df61
Anderson Assis <anderson.assis1989@yahoo.com>
2022-07-25 22:03:23 -0400

    First Commit

---
+++ rest-API/src/main/java/assisanderson/restAPI/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package assisanderson.restAPI.repository;
+
+import assisanderson.restAPI.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
+
+}
